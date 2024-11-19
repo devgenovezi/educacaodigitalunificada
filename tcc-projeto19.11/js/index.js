8213afed0e0380adaa70da7b463d21f45094f5b9
devgenovezi <dev.genovezi@gmail.com>
2024-11-19 17:45:23 -0300

    teste-commit11.19

---
+++ tcc-projeto19.11/js/index.js
@@ -0,0 +1,46 @@
+// js do nav-mobile
+const navbar = document.querySelector('.navbar');
+const mobileNavbar = document.querySelector('.navbar__mobile');
+const button = document.querySelector('.burguer');
+
+button.addEventListener('click', function () {
+  mobileNavbar.classList.toggle('active');
+});
+
+window.addEventListener('scroll', function () {
+  if (this.window.pageYOffset > 0) return navbar.classList.add('active');
+  return navbar.classList.remove('active');
+});
+
+// section carousel
+const slides = document.querySelectorAll('.carousel-slide');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+let currentSlide = 0;
+
+// Função para mostrar o slide
+function showSlide(index) {
+  if (index >= slides.length) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = slides.length - 1;
+  } else {
+    currentSlide = index;
+  }
+  const offset = -currentSlide * 100; // Movimenta o container do carrossel
+  document.querySelector('.carousel-container').style.transform = `translateX(${offset}%)`;
+}
+
+// Controle manual dos botões
+prevBtn.addEventListener('click', () => {
+  showSlide(currentSlide - 1);
+});
+
+nextBtn.addEventListener('click', () => {
+  showSlide(currentSlide + 1);
+});
+
+// Troca automática dos slides a cada 5 segundos
+setInterval(() => {
+  showSlide(currentSlide + 1);
+}, 5000);
